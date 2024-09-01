7625d2509f128a8e4e010a47f5a3922b17b03033
itsmediahn <hardiantiarfah@gmail.com>
2024-09-01 05:14:40 -0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = 'CGP Angkatan 10 Kab. Luwu Utara Kelas 10.121.SULSEL have something for you'.split('')
+const text = 'From CGP Angkatan 10 Kab. Luwu Utara Kelas 10.121.SULSEL'.split('')
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
     title.innerHTML += `<span>${text[index]}<span/>`
