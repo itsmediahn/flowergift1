bb9f36e063e45153ab2f2179a315f74a4b6274a4
itsmediahn <hardiantiarfah@gmail.com>
2024-09-01 05:08:03 -0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = 'I Have Something'.split('')
+const text = 'CGP Angkatan 10 Kab. Luwu Utara Kelas 10.121.SULSEL have something for you'.split('')
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
     title.innerHTML += `<span>${text[index]}<span/>`
