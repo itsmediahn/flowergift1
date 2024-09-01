b50a3ae2878f47d5ad967cc2dcd3fdd30324f67a
itsmediahn <hardiantiarfah@gmail.com>
2024-09-01 05:27:04 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,8 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('Thankyou Verymuch 
-                    We Love You Bu Nurhana').split('')
+    const titles = ('We Love You Bu Nurhana').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
